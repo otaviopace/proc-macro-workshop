fc0215dee7ef1b45f3c0bf4b901abbcab0aa702c
Otávio Pace <otaviopp8@gmail.com>
2020-07-12 18:16:11 -0300

    builder: test 1 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,16 @@
 extern crate proc_macro;
 
+use quote::quote;
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let expanded = quote! {
+
+    };
+
+    expanded.into()
 }
