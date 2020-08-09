353d53ab44cf3b987497b00f23e1174b7ec1ef25
Otavio Pace <otaviopp8@gmail.com>
2020-08-09 15:14:52 -0300

    builder: tests 4 and 5 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -16,6 +16,24 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let fields_with_option = get_fields_with_option(&input.data);
     let none_fields = get_none_fields(&input.data);
     let methods = get_builder_methods(&input.data);
+    let struct_fields_from_builder = convert_builder_fields(&input.data);
+
+    let builder_error = quote! {
+        #[derive(Debug)]
+        pub enum BuilderError {
+            NoneField(String),
+        }
+
+        impl std::error::Error for BuilderError {}
+
+        impl std::fmt::Display for BuilderError {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    Self::NoneField(s) => write!(f, "BuilderError::NoneField({})", s)
+                }
+            }
+        }
+    };
 
     let expanded = quote! {
         pub struct #builder_name { #fields_with_option }
@@ -30,7 +48,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_name {
             #methods
+
+            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+                Ok(#struct_name {
+                    #struct_fields_from_builder
+                })
+            }
         }
+
+        #builder_error
     };
 
     expanded.into()
@@ -88,6 +114,28 @@ fn get_builder_methods(data: &Data) -> TokenStream2 {
                                 self.#name = Some(#name);
                                 self
                             }
+                        }
+                    }).collect()
+                },
+                _ => unimplemented!(),
+            }
+        },
+        _ => unimplemented!(),
+    }
+}
+
+fn convert_builder_fields(data: &Data) -> TokenStream2 {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    fields.named.iter().map(|f| {
+                        let name = &f.ident;
+                        quote! {
+                            #name: self.#name
+                                .as_ref()
+                                .ok_or_else(|| BuilderError::NoneField(stringify!(#name).to_string()))?
+                                .clone(),
                         }
                     }).collect()
                 },
