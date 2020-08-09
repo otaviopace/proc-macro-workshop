69863d4dd1fd0b4d71c44748ea5e30bcf5c6a7d0
Otavio Pace <otaviopp8@gmail.com>
2020-08-09 16:54:33 -0300

    builder: test 6 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,7 +3,7 @@ extern crate proc_macro;
 use quote::quote;
 use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenStream as TokenStream2};
-use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields};
+use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields, Type, PathArguments, GenericArgument};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -70,6 +70,7 @@ fn get_fields_with_option(data: &Data) -> TokenStream2 {
                     fields.named.iter().map(|f| {
                         let name = &f.ident;
                         let ty = &f.ty;
+                        let ty = extract_type_from_option(&ty);
                         quote! {
                             #name: Option<#ty>,
                         }
@@ -101,6 +102,42 @@ fn get_none_fields(data: &Data) -> TokenStream2 {
     }
 }
 
+fn is_option(ty: &Type) -> bool {
+    if let Type::Path(typepath) = ty {
+        if let Some(segment) = typepath.path.segments.last() {
+            if segment.ident == "Option" {
+                if let PathArguments::AngleBracketed(ref generic_args) = segment.arguments {
+                    if let Some(generic_arg) = generic_args.args.first() {
+                        if let GenericArgument::Type(_) = generic_arg {
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    false
+}
+
+fn extract_type_from_option(ty: &Type) -> Type {
+    if let Type::Path(typepath) = ty {
+        if let Some(segment) = typepath.path.segments.last() {
+            if segment.ident == "Option" {
+                if let PathArguments::AngleBracketed(ref generic_args) = segment.arguments {
+                    if let Some(generic_arg) = generic_args.args.first() {
+                        if let GenericArgument::Type(t) = generic_arg {
+                            return t.clone()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    ty.clone()
+}
+
 fn get_builder_methods(data: &Data) -> TokenStream2 {
     match *data {
         Data::Struct(ref data) => {
@@ -109,6 +146,8 @@ fn get_builder_methods(data: &Data) -> TokenStream2 {
                     fields.named.iter().map(|f| {
                         let name = &f.ident;
                         let ty = &f.ty;
+
+                        let ty = extract_type_from_option(&ty);
                         quote! {
                             pub fn #name(&mut self, #name: #ty) -> &mut Self {
                                 self.#name = Some(#name);
@@ -131,11 +170,18 @@ fn convert_builder_fields(data: &Data) -> TokenStream2 {
                 Fields::Named(ref fields) => {
                     fields.named.iter().map(|f| {
                         let name = &f.ident;
-                        quote! {
-                            #name: self.#name
+                        let ty = &f.ty;
+                        if is_option(ty) {
+                            quote! {
+                                #name: self.#name.clone(),
+                            }
+                        } else {
+                            quote! {
+                                #name: self.#name
                                 .as_ref()
-                                .ok_or_else(|| BuilderError::NoneField(stringify!(#name).to_string()))?
-                                .clone(),
+                                    .ok_or_else(|| BuilderError::NoneField(stringify!(#name).to_string()))?
+                                    .clone(),
+                            }
                         }
                     }).collect()
                 },
